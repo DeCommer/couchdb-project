64ebffbf28bde14e3b5518a63f01a3e6a3bac9fb
Joseph DeCommer <jdecomer@gmail.com>
2019-10-01 17:36:08 -0400

    Ported to nano CouchDB driver

--- app.js
+++ app.js
@@ -1,50 +1,9 @@
-const express = require('express');
+const express   = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
-const NodeCouchDB = require('node-couchdb');
-
-// const couch = new NodeCouchDB({
-//     host: 'process.env.HOST',
-//     auth: {
-//         user: 'process.env.USER',
-//         password: process.env.DBPASS,
-//     }
-// })
-
-// var Cloudant = require('@cloudant/cloudant');
-
-// var cloudant = new Cloudant({
-//   account: '',
-//   plugins: {
-//     iamauth: {
-//       iamApiKey: ''
-//     }
-//   }
-// });
-// cloudant.db.list(function(err, body) {
-//   body.forEach(function(db) {
-//     console.log(db);
-//   });
-// });
-
-const couch = new NodeCouchDB({
-    host: '10.0.0.6',
-    port: 5984,
-    // auth: {
-    //     user: 'joseph',
-    //     password: process.env.DBPASS,
-    // }
-})
-
-const dbName = 'people';
-const viewUrl = '_all_docs?include_docs=true'
-
-// couch.listDatabases()
-// .then((dbs) => {
-//     console.log(dbs[4]);
-// })
-
+const nano = require('nano')('http://localhost:5984');
+const people = nano.use('people')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -56,58 +15,53 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-    couch.get(dbName, viewUrl)
-    .then(({data, headers, status}) => {
-        // console.log(data)
-        res.render('index', {
-            people:data.rows
+    people.list({include_docs: true})
+        .then((data) => {
+            // console.log(data.rows)
+            res.render('index', {
+                people:data.rows
+            })
         })
+        .catch(err => {
+            console.log(err);
+        });
     })
-    .catch(err => {
-        console.log(err);
-    });
-});
 
 app.post('/people/add' , (req, res) => {
     const fname = req.body.fname;
-    const lname = req.body.lname;
-    const email = req.body.email
-    const street = req.body.street
-    const city = req.body.city
-    const state = req.body.state
-    couch.uniqid().then((ids) => {
-        const id = ids[0];
-        couch.insert(dbName,  {
-            _id: id,
-            fname: fname,
-            lname: lname,
-            email: email,
-            address: {
-                street: street, 
-                city: city, 
-                state: state
-            }
-        })
-        .then(({data, headers, status}) => {
+          lname = req.body.lname;
+          email = req.body.email;
+          street = req.body.street;
+          city = req.body.city;
+          state = req.body.state;
+    people.insert({
+        fname: fname,
+        lname: lname,
+        email: email,
+        address: {
+            street: street, 
+            city: city, 
+            state: state
+        }
+    })
+        .then(() => {
             res.redirect('/');
         })
         .catch(err => {
             console.log(err);
         })
     })
-})
 
 app.post('/people/delete/:id', (req, res) => {
     let id = req.params.id;
     let rev = req.body.rev;
-    couch.del(dbName, id, rev)
-    .then((data, headers, status)=> {
-        res.redirect('/')
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
+    people.destroy(id, rev)
+        .then((data)=> {
+            res.redirect('/')
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
 const port = process.env.PORT || 3000;
